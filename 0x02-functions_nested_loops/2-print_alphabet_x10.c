e9d425dda2cc7aa92910bf052cca768ca7e13103
Bassey Victoria <vicrooneybassey@gmail.com>
2023-07-18 15:48:13 -0700

    next function (made fixes)

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the alphabet 10 times, in lowercase, followed by a new line.
+ * print_alphabet_x10 - Prints the alphabet 10 times, in lowercase.
  *
  * Return: Always 0 (Success)
  */
